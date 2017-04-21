641dbc59e36952f91bbe1658f2ac46b610ad283e
khex <o.kholjavko@yandex.ua>
2017-04-21 23:02:22 +0300

    smth

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 //  Include gulp
-var gulp = require('gulp');
-var jade = require('gulp-jade');
+var gulp      = require('gulp');
+var jade      = require('gulp-jade');
 var uglifycss = require('gulp-uglifycss');
-var prty = require('gulp-prettify');
-var less = require('gulp-less');
+var prty      = require('gulp-prettify');
+var less      = require('gulp-less');
 
 gulp.task('less', function () {
     return gulp
@@ -17,16 +17,17 @@ gulp.task('jade', function() {
     // do not work
     var YOUR_LOCALS = { pretty: true };
 
-    gulp.src([
+    gulp
+    .src([
         './templates/index.jade',
         './templates/kafe.jade',
         './templates/hotel.jade',
         './templates/kolyby.jade',
         './templates/stolyky.jade',
-        ])
-        .pipe(jade({ locals: YOUR_LOCALS }))
-        // .pipe(prty({indent_size: 2}))
-        .pipe(gulp.dest(''));
+    ])
+    .pipe(jade({ locals: YOUR_LOCALS }))
+    .pipe(prty({indent_size: 2}))
+    .pipe(gulp.dest(''));
 });
 
 // old
