81e63bc391f735a7b88cadb8870f34fb35773de5
khex <khex@ua.fm>
2020-05-18 00:22:41 +0300

    Auto-generated commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+//  Include gulp
+var gulp      = require('gulp');
+var jade      = require('gulp-jade');
+var uglifycss = require('gulp-uglifycss');
+var prty      = require('gulp-prettify');
+var less      = require('gulp-less');
+
+gulp.task('less', function () {
+    return gulp
+    .src('./stylesheets/albatros.less')
+    .pipe(less())
+    .pipe(uglifycss({ 'uglyComments': true }))
+    .pipe(gulp.dest('./stylesheets'));
+});
+
+gulp.task('jade', function() {
+    // do not work
+    var YOUR_LOCALS = { pretty: true };
+
+    gulp
+    .src([
+        './templates/index.jade',
+        './templates/kafe.jade',
+        './templates/hotel.jade',
+        './templates/kolyby.jade',
+        './templates/stolyky.jade',
+    ])
+    .pipe(jade({ locals: YOUR_LOCALS }))
+    .pipe(prty({indent_size: 2}))
+    .pipe(gulp.dest(''));
+});
+
+// old
+//  gulp.task('default', ['less', 'jade']);
+
+gulp.task('default', ['jade']);
