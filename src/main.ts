631a444d243ad417a665282ca92b7a71113812d9
khex <khex@ua.fm>
2023-03-01 15:06:49 +0200

    chore

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ import {
 import { AppModule }         from './app/app.module';
 import { environment }       from './environments/environment';
 
-
 if (environment.production) {
   enableProdMode();
 }
